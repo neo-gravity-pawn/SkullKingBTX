387a56bdf3d53ffc5686c5b8b7ad490acc5c3084
bobthebit <maretro@gmx.de>
2020-05-19 16:09:39 +0200

    #5 added all cards to stock

--- backend/src/core/stock.ts
+++ backend/src/core/stock.ts
@@ -1,12 +1,47 @@
-import { Card } from "./card";
+import { MutableCard, MutableCardType } from './mutableCard';
+import { Card, CardColor, CardType } from "./card";
 
 export class Stock {
     private cards: Array<Card>;
     constructor() {
         this.cards = new Array<Card>();
+        this.addColorCards();
+        this.addOtherCards();
+    }
+
+    private addColorCards() {
+        const colors = [CardColor.red, CardColor.blue, CardColor.yellow, CardColor.black];
+        colors.forEach( (color: CardColor) => {
+            for (let value = 1; value <= 13; value ++) {
+                this.cards.push(
+                    new Card({type: (color === CardColor.black) ? CardType.trump : CardType.color , color, value })
+                );
+            }
+        })      
+    }
+
+    private addOtherCards() {
+        for (let i = 0; i < 5; i++) {
+            this.cards.push(new Card({type: CardType.pirate}));
+            this.cards.push(new Card({type: CardType.escape}));
+        }
+        for (let i = 0; i < 2; i++) {
+            this.cards.push(new Card({type: CardType.mermaid}));
+        }
+        this.cards.push(new Card({type: CardType.skullking}));
+        this.cards.push(new MutableCard(MutableCardType.scaryMary,
+            [new Card({type: CardType.pirate}), new Card({type: CardType.escape})]
+        ));
     }
 
     public getNumberOfCards() {
         return this.cards.length;
+    }
+
+    public getCard(index: number) {
+        if (index < 0 || index >= this.cards.length) {
+            throw Error('Stock can only provide cards within range [0, '+this.cards.length + ']');
+        }
+        return this.cards[index];
     }
 }
--- backend/tests/stock.spec.ts
+++ backend/tests/stock.spec.ts
@@ -7,4 +7,25 @@ describe('Stock', () => {
         const s = new Stock();
         expect(s.getNumberOfCards()).to.be.equal(66);
     })
+    it('cards should be accessible', () => {
+        const s = new Stock();
+        const c = s.getCard(0);
+        const c2 = s.getCard(65);
+    })
+    it('should throw error if invalid cards are accessed', () => {
+        const s = new Stock();
+        expect(() => {
+            const c = s.getCard(-1);
+        }).to.throw();
+        expect(() => {
+            const c = s.getCard(66);
+        }).to.throw();
+    })
+
+    it('should contain 65 standard cards and one mutable card', () => {
+        const s = new Stock();
+        for (let i = 0; i < s.getNumberOfCards(); i++) {
+            // const c = 
+        }
+    })
 })
