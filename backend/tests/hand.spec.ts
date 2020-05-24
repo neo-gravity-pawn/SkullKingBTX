f22ba61ca16947c422a481c90c3e220ffe8f7d81
bobthebit <maretro@gmx.de>
2020-05-24 18:18:27 +0200

    #9 added card setup for testing card ordering

---
+++ backend/tests/hand.spec.ts
@@ -0,0 +1,25 @@
+import { MutableCardType, MutableCard } from '@core/mutableCard';
+import { CardType, CardColor, Card } from '@core/card';
+import { CardCollection } from '@core/cardCollection';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Hand', () => {
+    it('should be sortable', () => {
+        const hand = new CardCollection();
+        const cards = [
+            new Card({type: CardType.color, color: CardColor.blue, value: 8}),
+            new Card({type: CardType.color, color: CardColor.blue, value: 1}),
+            new Card({type: CardType.color, color: CardColor.blue, value: 13}),
+            new Card({type: CardType.color, color: CardColor.red, value: 10}),
+            new Card({type: CardType.color, color: CardColor.red, value: 2}),
+            new Card({type: CardType.color, color: CardColor.blue, value: 13}),
+            new Card({type: CardType.pirate}),
+            new Card({type: CardType.pirate}),
+            new Card({type: CardType.skullking}),
+            new Card({type: CardType.mermaid}),
+            new MutableCard(MutableCardType.scaryMary, [new Card({type: CardType.escape}), new Card({type: CardType.pirate})]),
+            new Card({type: CardType.escape})
+        ]
+    })
+})
