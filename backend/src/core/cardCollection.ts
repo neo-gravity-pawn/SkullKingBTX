99dfa76637921f0015bbe64028229253cb4b5436
bobthebit <maretro@gmx.de>
2020-05-24 20:16:22 +0200

    #9 finished sorting: sorted by type (with respect to card power), colors also sorted by value and by number of available cards per color

--- backend/src/core/cardCollection.ts
+++ backend/src/core/cardCollection.ts
@@ -1,6 +1,6 @@
 import { isMutableCard, MutableCard } from '@core/mutableCard';
-import { printCard } from '@helper/output';
-import { Card, CardType } from "./card";
+import { printCard, printCollection } from '@helper/output';
+import { Card, CardType, CardColor } from "./card";
 
 const cardTypeOrder = [
     CardType.skullking,
@@ -48,10 +48,40 @@ export class CardCollection {
     }
 
     public sort() {
-        this.cards.sort((a, b) => {
+        const valueCards = [
+            CardType.trump,
+            CardType.color
+        ]
+        const colorCount = {
+            [CardColor.red]: 0,
+            [CardColor.blue]: 0,
+            [CardColor.yellow]: 0,
+            [CardColor.black]: 0,
+            [CardColor.none]: 0
+        }
+        this.cards.forEach(card => {
+            colorCount[card.color] ++;
+        });
+        
+        const cards = this.cards.filter( a => valueCards.indexOf(a.type) === -1 );
+        const vCards = this.cards.filter( a => valueCards.indexOf(a.type) !== -1);
+        cards.sort((a, b) => {
             const aType = isMutableCard(a) ? (a as MutableCard).mutableType : a.type;
             const bType = isMutableCard(b) ? (b as MutableCard).mutableType : b.type;
             return (cardTypeOrder.indexOf(aType) - cardTypeOrder.indexOf(bType));
         });
+        vCards.sort((a,b) => {
+            if (a.type !== b.type) {
+                return valueCards.indexOf(a.type) - valueCards.indexOf(b.type);
+            } else {
+                if (a.color === b.color) {
+                    return b.value - a.value;
+                }
+                else {
+                    return colorCount[b.color] - colorCount[a.color];
+                }
+            }
+        })
+        this.cards = [...cards, ...vCards];
     }
 }
--- backend/tests/hand.spec.ts
+++ backend/tests/hand.spec.ts
@@ -1,3 +1,4 @@
+import { printCollection } from '@helper/output';
 import { MutableCard } from '@core/mutableCard';
 import { CardType, CardColor, Card } from '@core/card';
 import { CardCollection } from '@core/cardCollection';
@@ -8,18 +9,21 @@ describe('Hand', () => {
     it('should be sortable', () => {
         const hand = new CardCollection();
         const cards = [
-            new Card({type: CardType.color, color: CardColor.blue, value: 8}),
-            new Card({type: CardType.color, color: CardColor.blue, value: 1}),
-            new Card({type: CardType.color, color: CardColor.blue, value: 13}),
-            new Card({type: CardType.color, color: CardColor.red, value: 10}),
-            new Card({type: CardType.color, color: CardColor.red, value: 2}),
-            new Card({type: CardType.color, color: CardColor.blue, value: 13}),
-            new Card({type: CardType.pirate}),
-            new Card({type: CardType.pirate}),
-            new Card({type: CardType.skullking}),
-            new Card({type: CardType.mermaid}),
+            new Card({type: CardType.color, color: CardColor.blue, value: 8}), //0
+            new Card({type: CardType.color, color: CardColor.red, value: 10}), //1
+            new Card({type: CardType.color, color: CardColor.yellow, value: 12}), //2
+            new Card({type: CardType.color, color: CardColor.red, value: 2}), //3
+            new Card({type: CardType.color, color: CardColor.blue, value: 1}), //4
+            new Card({type: CardType.color, color: CardColor.blue, value: 13}), //5
+            new Card({type: CardType.pirate}), //6
+            new Card({type: CardType.pirate}), //7
+            new Card({type: CardType.skullking}), //8
+            new Card({type: CardType.mermaid}), //9
             new MutableCard(CardType.scarymary, [new Card({type: CardType.escape}), new Card({type: CardType.pirate})]),
-            new Card({type: CardType.escape})
+            new Card({type: CardType.escape}), //11
+            new Card({type: CardType.trump, value: 2}), //12
+            new Card({type: CardType.trump, value: 9}), //13
+
         ]
         cards.forEach( card => {
             hand.addCard(card);
@@ -31,9 +35,15 @@ describe('Hand', () => {
             hand.getCard(2) === cards[6] &&
             hand.getCard(3) === cards[7] &&
             hand.getCard(4) === cards[9] &&
-            hand.getCard(4) === cards[9] &&
-            hand.getCard(11) === cards[11]
-
+            hand.getCard(5) === cards[11] &&
+            hand.getCard(6) === cards[13] &&
+            hand.getCard(7) === cards[12] &&
+            hand.getCard(8) === cards[5] &&
+            hand.getCard(9) === cards[0] &&
+            hand.getCard(10) === cards[4] &&
+            hand.getCard(11) === cards[1] &&
+            hand.getCard(12) === cards[3] &&
+            hand.getCard(13) === cards[2]
         ).to.be.true;
     })
 })
