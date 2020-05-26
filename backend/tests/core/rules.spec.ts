8f50c8a1791e89d67b61342d08f4c490793e4aa1
bobthebit <maretro@gmx.de>
2020-05-26 12:24:50 +0200

    #6 extended and restructured rule test

--- backend/tests/core/rules.spec.ts
+++ backend/tests/core/rules.spec.ts
@@ -5,26 +5,36 @@ import { CardCollection } from '@core/cardCollection';
 import { canBeAddedToTrick } from '@core/rules.ts';
 import { cc } from '@helper/create';
 
+let trick: CardCollection;
+let hand: CardCollection;
 
-const cr8 = cc('cr8');
-const cr3 = cc('cr3');
-const cy11 = cc('cy11');
-const cr9 = cc('cr9');
-const cy7 = cc('cy7');
-
-
+function setup() {
+    [trick, hand] = [new CardCollection(), new CardCollection()]
+}
 
 describe('Trick', () => {
+    it('should trow error if invalid hand car is selected', () => {
+        setup();
+        expect(() => {
+            canBeAddedToTrick(hand, 1, trick);
+        }).to.throw;
+    })
     it('should check, if color rules applies', () => {
-        const hand = new CardCollection();
-        hand.addCard(cr9);
-        hand.addCard(cy7);
+        setup();
+        hand.addCard(cc('cr9'));
+        hand.addCard(cc('cy7'));
 
-        const trick = new CardCollection();
-        trick.addCard(cr8);
-        trick.addCard(cy11);
+        expect(canBeAddedToTrick(hand, 1, trick)).to.be.true;
+
+        trick.addCard(cc('cr8'));
+        trick.addCard(cc('cy11'));
 
         expect(canBeAddedToTrick(hand, 1, trick)).to.be.false;
+
+        hand.removeCard(0);
+        hand.addCard(cc('cy6'));
+
+        expect(canBeAddedToTrick(hand, 0, trick)).to.be.true;
 
     })
 })
